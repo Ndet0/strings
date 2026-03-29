710d01118c2d5c7de55c8b2b7f85a4336f15119a
Ndet0 <festusndet00@gmail.com>
2026-03-29 15:39:56 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code in this file!
+const currentUser = "Festus Ndeto";
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.split(" ").map(name =>name[0].toUpperCase()).join("")}!`;
+
+
+console.log(welcomeMessage);
+console.log(excitedWelcomeMessage);
+console.log(shortGreeting);
