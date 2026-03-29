1705d216c913b60fd1d758502eb55e376104e61e
Ndet0 <festusndet00@gmail.com>
2026-03-29 20:38:47 +0300

    greetuser

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 // Write your code in this file!
-const currentUser = "Festus Ndeto";
 
-const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+function greetUser(name){
+    return `Welcome, ${name.split(" ").map(name => name[0].toUpperCase()).join("")}!`;
+}
 
-const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
 
-const shortGreeting = `Welcome, ${currentUser.split(" ").map(name =>name[0].toUpperCase()).join("")}!`;
+readline.question('Enter your name:', (name=>{
+    console.log(greetUser(name));
+    readline.close();
+}));
+
+// const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+// const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+// const shortGreeting = `Welcome, ${currentUser.split(" ").map(name =>name[0].toUpperCase()).join("")}!`;
 
 
-console.log(welcomeMessage);
-console.log(excitedWelcomeMessage);
-console.log(shortGreeting);
+
+// console.log(welcomeMessage);
+// console.log(excitedWelcomeMessage);
+// console.log(shortGreeting);
